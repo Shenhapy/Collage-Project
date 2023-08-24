c6d4c8acaff089fdf6f767495f1a469dc6703eca
Ibrahim Elshenhapy <ielsh091@uottawa.ca>
2023-08-25 01:23:52 +0300

    Add files via upload

---
+++ Data Aquisition/Labs/Report/UploadedFlow/flowmetercode/flowmetercode.ino
@@ -0,0 +1,24 @@
+volatile int flow_frequency; float vol = 0.0,l_minute;
+unsigned char flowsensor = 2; unsigned long currentTime;unsigned long cloopTime;
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 9);
+void flow () {flow_frequency++;}
+void setup(){pinMode(flowsensor, INPUT);
+   digitalWrite(flowsensor, HIGH);Serial.begin(9600); lcd.begin(16, 2);
+   attachInterrupt(digitalPinToInterrupt(flowsensor), flow, RISING);
+   lcd.clear(); lcd.setCursor(0,0); lcd.print("Water Flow Meter");
+   lcd.setCursor(0,1); lcd.print("Circuit Digest");
+   currentTime = millis(); cloopTime = currentTime; }
+void loop (){currentTime = millis();
+   if(currentTime >= (cloopTime + 1000))
+   {cloopTime = currentTime; 
+    if(flow_frequency != 0){l_minute = (flow_frequency / 7.5);
+      lcd.clear();lcd.setCursor(0,0);lcd.print("Rate: ");lcd.print(l_minute);
+      lcd.print(" L/M");l_minute = l_minute/60;
+      lcd.setCursor(0,1); vol = vol +l_minute; lcd.print("Vol:");  lcd.print(vol);
+      lcd.print(" L"); flow_frequency = 0; 
+      Serial.print(l_minute, DEC); Serial.println(" L/Sec");}
+      else {Serial.println(" flow rate = 0 ");
+      lcd.clear();lcd.setCursor(0,0);lcd.print("Rate: ");
+      lcd.print( flow_frequency );lcd.print(" L/M");
+      lcd.setCursor(0,1);lcd.print("Vol:");lcd.print(vol);lcd.print(" L");}}}
---
+++ Data Aquisition/Labs/Report/UploadedLevel/UltraSonic_simulation/UltraSonic_simulation.ino
@@ -0,0 +1,24 @@
+const int echoPin = 2; 
+const int pingPin = 3;  
+void setup() 
+{ Serial.begin(9600);
+  pinMode(pingPin, OUTPUT); pinMode(echoPin, INPUT); }
+void loop()
+{ long duration, inches, cm; 
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(pingPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
+  cm = microsecondsToCentimeters(duration); 
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(100);}
+long microsecondsToInches(long microseconds) 
+{return microseconds / 74 / 2;}
+long microsecondsToCentimeters(long microseconds) 
+{return microseconds / 29 / 2;}
